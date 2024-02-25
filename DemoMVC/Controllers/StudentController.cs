51db2a1d6415e3fd7f2e7b2768db1a54f5f85252
Trungdoan <2021050195@student.humg.edu.vn>
2024-02-25 11:09:15 +0700

    student

---
+++ DemoMVC/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace DemoMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Student std)
+        {
+            string strResult = "Hello :" + std.StudentID + "-" + std.TenSV ; 
+            ViewBag.Nhandulieu = strResult;
+            return View();
+        }
+    }
+}
